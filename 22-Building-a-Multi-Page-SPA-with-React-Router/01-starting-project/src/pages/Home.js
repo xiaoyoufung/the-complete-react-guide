941042b07ec694db8087bd0488ad48a74e858981
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-24 21:52:41 +0700

    352. Defining & Using Dynamic Routes

--- 22-Building-a-Multi-Page-SPA-with-React-Router/01-starting-project/src/pages/Home.js
+++ 22-Building-a-Multi-Page-SPA-with-React-Router/01-starting-project/src/pages/Home.js
@@ -1,9 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function HomePage() {
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        // navigate('/products');
+        navigate('/products', { replace: true });
+    }
+
     return (<>
         <h1>My Home Page</h1>
         <p>Go to <Link to="/products">the list of products</Link>.</p>
+        <p>
+            <button onClick={navigateHandler}>Go to Products</button>
+        </p>
     </>);
 }
 
