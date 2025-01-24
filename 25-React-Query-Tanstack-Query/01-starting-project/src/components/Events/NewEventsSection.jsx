d347c8f8e09a18298d4cd112aed753574c8e5e97
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-24 15:16:44 +0700

    425. Using the Query Key As Query Function Input

--- 25-React-Query-Tanstack-Query/01-starting-project/src/components/Events/NewEventsSection.jsx
+++ 25-React-Query-Tanstack-Query/01-starting-project/src/components/Events/NewEventsSection.jsx
@@ -7,8 +7,8 @@ import EventItem from "./EventItem.jsx";
 
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ["events"],
-    queryFn: fetchEvents,
+    queryKey: ["events", { max: 3 }],
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
     staleTime: 5000,
     // gcTime: 60000,
   });
@@ -21,7 +21,10 @@ export default function NewEventsSection() {
 
   if (isError) {
     content = (
-      <ErrorBlock title="An error occurred" message={error.info?.message || 'Failed to fetch events.'} />
+      <ErrorBlock
+        title="An error occurred"
+        message={error.info?.message || "Failed to fetch events."}
+      />
     );
   }
 
