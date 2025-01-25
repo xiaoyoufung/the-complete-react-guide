1353da2e9a685b8d3de1a29f220e962f8f158df2
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-25 13:58:23 +0700

    357. Time to Practice: 1. Add five new (dummy) page components

---
+++ 22-Building-a-Multi-Page-SPA-with-React-Router/12-adv-starting-project/frontend/src/pages/EditEvent.js
@@ -0,0 +1,9 @@
+function EditEventPage() {
+  return (
+    <div>
+      <h1>Edit Event</h1>
+    </div>
+  );
+}
+
+export default EditEventPage;
---
+++ 22-Building-a-Multi-Page-SPA-with-React-Router/12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -0,0 +1,9 @@
+function NewEventPage() {
+  return (
+    <div>
+      <h1>New Event Page</h1>
+    </div>
+  );
+}
+
+export default NewEventPage;
