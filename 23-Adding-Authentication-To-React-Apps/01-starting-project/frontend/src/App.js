fca05b5f2ab937e087281418890b6c874eb8095e
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-26 14:52:04 +0700

    389. Implementing the Auth Action

--- 23-Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/App.js
+++ 23-Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/App.js
@@ -13,7 +13,7 @@ import NewEventPage from './pages/NewEvent';
 import RootLayout from './pages/Root';
 import { action as manipulateEventAction } from './components/EventForm';
 import NewsletterPage, { action as newsletterAction } from './pages/Newsletter';
-import AuthPage from './pages/Authentication';
+import AuthPage, {action as authAction} from './pages/Authentication';
 
 const router = createBrowserRouter([
   {
@@ -63,6 +63,7 @@ const router = createBrowserRouter([
       {
         path: 'auth',
         element: <AuthPage />,
+        action: authAction,
       }
     ],
   },
--- 23-Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/pages/Authentication.js
+++ 23-Adding-Authentication-To-React-Apps/01-starting-project/frontend/src/pages/Authentication.js
@@ -1,7 +1,43 @@
 import AuthForm from '../components/AuthForm';
+import { redirect } from 'react-router-dom';
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({ request }) {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw new Response(JSON.stringify({ message: 'Unsupported mode.' }),
+      { status: 422 });
+  }
+
+  const data = await request.formData();
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password'),
+  };
+
+  const response = await fetch(`http://localhost:8080/${mode}`, {
+    method: 'POST',
+    body: JSON.stringify(authData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if(response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if(!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Authentication failed.' }),
+      { status: 500 });
+  }
+
+  return redirect('/');
+}
