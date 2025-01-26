05d22cda1bbd6eda899163531d3268c8f8a66c7a
Xiaoyou Fung <109220555+xiaoyoufung@users.noreply.github.com>
2025-01-26 11:34:59 +0700

    381. Controlling Which Data Should Be Deferred

--- 22-Building-a-Multi-Page-SPA-with-React-Router/12-adv-starting-project/frontend/src/pages/EventDetail.js
+++ 22-Building-a-Multi-Page-SPA-with-React-Router/12-adv-starting-project/frontend/src/pages/EventDetail.js
@@ -1,33 +1,61 @@
+import { Suspense } from "react";
 import EventItem from "../components/EventItem";
-import { useRouteLoaderData, redirect } from "react-router-dom";
+import { useRouteLoaderData, redirect, Await } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventDetailPage() {
-    const data = useRouteLoaderData('event-detail');
+    const { event, events } = useRouteLoaderData('event-detail');
 
-    let content;
-
-    if (data) {
-        content = <EventItem event={data.event} />;
-    }
-
-    return <>
-        <h1>Event Detail Page</h1>
-        {content}
-    </>
+    return (
+        <>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={event}>
+                    {(loadedEvent) => <EventItem event={loadedEvent} />}
+                </Await>
+            </Suspense>
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={events}>
+                    {(loadedEvents) => <EventsList events={loadedEvents} />}
+                </Await>
+            </Suspense>
+        </>
+    );
 }
 
 export default EventDetailPage;
 
-export async function loader({ request, params }) {
-    const id = params.eventId;
-
+async function loadEvent(id) {
     const response = await fetch(`http://localhost:8080/events/${id}`);
 
     if (!response.ok) {
         throw new Response(JSON.stringify({ message: 'Could not fetch details for selected event.' }),
             { status: 500 });
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.event;
+    }
+}
+
+// should be in a separate file
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+
+    if (!response.ok) {
+        //   return {isError: true, message: 'Could not fetch events'};
+        throw new Response(JSON.stringify({ message: 'Could not fetch events' }),
+            { status: 500 });
+    } else {
+        const resData = await response.json();
+        return resData.events;
+    }
+}
+
+export async function loader({ request, params }) {
+    const id = params.eventId;
+
+    return {
+        event: await loadEvent(id),
+        events: loadEvents(),
     }
 }
 
@@ -35,10 +63,10 @@ export async function action({ params, request }) {
     const eventId = params.eventId;
     const response = await fetch(`http://localhost:8080/events/${eventId}`, {
         method: request.method,
-        
+
     });
 
-    if(!response.ok) {
+    if (!response.ok) {
         throw new Response(JSON.stringify({ message: 'Could not delete the event.' }),
             { status: 500 });
     }
